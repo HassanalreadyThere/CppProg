764df207448878ea66bd007502f277514afa5fc1
Sathish Sarma <sathishsarma.edu@gmail.com>
2025-08-15 14:58:24 +0800

    File name changed

---
+++ 01-Hello World/HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Simple C++ program
+// FileName: HelloWorld.cpp
+
+#include <iostream> // for std::cout
+
+int main() {
+	
+    // Prints "Hello, World" to the terminal window.
+    std::cout << "Hello, World" << std::endl;
+	return 0; // exit status code
+}
---
+++ 02-Best Practice/BestPractice.cpp
@@ -0,0 +1,29 @@
+/**********************************************************************
+
+Program Title : Best Practices of Programming
+FileName: "BestPractice.cpp"
+
+Programmer : Sathish Sarma
+Contact Details : sathish.sarma@newinti.edu.my
+Date : 14 August 2025
+
+**********************************************************************/
+
+#include <iostream> // for std::cout, std::endl
+
+int main() {
+    // Best practices of programming
+    std::cout << "Hi, I'm Programmer Sathish Sarma.\n" << std::endl;
+    std::cout << "A good application should have following best practices of programming" << std::endl;
+    std::cout << "1. Clear Structure" << std::endl;
+    std::cout << "2. Identifiers with meaningful names" << std::endl;
+    std::cout << "3. Comments for providing context and understanding" << std::endl;
+    std::cout << "4. Broken down the complex program into manageable components" << std::endl;
+    std::cout << "5. Exception handling" << std::endl;
+    std::cout << "6. Follow Coding Standards" << std::endl;
+    std::cout << "7. Readable and Extensible" << std::endl;
+    std::cout << std::endl;
+    std::cout << "What other aspects that you can think of for the best practices in programming?" << std::endl;
+
+    return 0; // exit status code
+}
